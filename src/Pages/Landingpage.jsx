d1e1a756e7f5ae9b7bc91a2ffb3e13765f7550e1
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2023-06-17 23:01:17 +0700

    Add Button on Landing Page

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 // import pokemonBallImage from "../Photo/pokeball-pokemon-removebg.png";
 
 const Landingpage = () => {
@@ -11,7 +11,24 @@ const Landingpage = () => {
         <div className="max-w-4xl mx-auto px-4 text-center">
           <h1 className="text-5xl font-bold text-whitet mb-8">Welcome to Pokédex NG</h1>
           <p className="text-xl font-semibold text-whitet mb-8">Discover and explore all your favorite Pokémon!</p>
-          <button onClick={() => navigate('/home')} className="bg-whitet text-landingpa hover:bg-primary hover:text-whitet text-lg font-bold py-3 px-6 duration-300 rounded-full shadow-lg">Get Started</button>
+          <div className="flex items-center justify-center flex-wrap flex-col">
+            <div>
+              <button onClick={() => navigate("/home")} className="bg-whitet text-landingpa hover:bg-primary hover:text-whitet text-lg font-bold py-3 px-6 duration-300 rounded-full shadow-lg">
+                Get Started
+              </button>
+            </div>
+            <div className="flex items-center justify-center flex-wrap">
+              <Link to={"https://pokeapi.co/docs/v2"} target="_blank">
+                <button className="bg-whitet text-landingpf hover:bg-landingpg hover:text-whitet text-lg font-bold py-3 px-6 mt-3 mx-2 duration-300 rounded-full shadow-lg">PokeApi</button>
+              </Link>
+              <Link to={"https://github.com/nehemiagueldi/pokedex-ng"} target="_blank">
+                <button className="bg-whitet text-landingpd hover:bg-landingpd hover:text-whitet text-lg font-bold py-3 px-6 mt-3 mx-2 duration-300 rounded-full shadow-lg">Github</button>
+              </Link>
+              <Link to={"https://github.com/PokeAPI/sprites"} target="_blank">
+                <button className="bg-whitet text-landingpf hover:bg-landingpg hover:text-whitet text-lg font-bold py-3 px-6 mt-3 mx-2 duration-300 rounded-full shadow-lg">PokeImg</button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
